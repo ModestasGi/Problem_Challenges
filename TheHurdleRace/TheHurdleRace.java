46202364bda9551eb78d9fe52081a9e4d9e067b2
Modestas <modestas.gir@gmail.com>
2018-11-11 15:47:50 +0100

    Add files via upload

---
+++ TheHurdleRace/TheHurdleRace.java
@@ -0,0 +1,45 @@
+package hackerrankchallenges.ProblemSolving;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TheHurdleRace {
+
+    // Complete the hurdleRace function below.
+    static int hurdleRace(int k, int[] height) {
+        Arrays.sort(height);
+        if(k < height[height.length-1]){
+        return height[height.length-1] - k;
+        }
+        return 0;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] height = new int[n];
+
+        String[] heightItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int heightItem = Integer.parseInt(heightItems[i]);
+            height[i] = heightItem;
+        }
+
+        int result = hurdleRace(k, height);
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+
+}
